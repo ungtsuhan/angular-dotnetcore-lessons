2d4ba601e96f911443a41f5b24b574a5f1a9232a
Ung Tsu Han <ungtsuhan@gmail.com>
2021-07-11 12:36:04 +0800

    create ICMPHealthCheck class

---
+++ Projects/HealthCheck/ICMPHealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheck
+{
+    public class ICMPHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
